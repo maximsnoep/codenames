98023a6e81ad73885842582b4beac02ccdcff266
Snoep <m.snoep@tue.nl>
2023-10-31 16:57:53 +0100

    change port for digital ocean

--- server.js
+++ server.js
@@ -77,6 +77,6 @@ io.on('connection', (socket) => {
 
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+server.listen(8080, () => {
+  console.log('Server is running on http://localhost:8080');
 });
