fe770804b6af95bcb51477559662b3b4d7f5967c
Snoep <ik@maximsnoep.com>
2024-03-17 17:33:19 +0100

    some update

--- server.js
+++ server.js
@@ -12,129 +12,196 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+server.listen(8080, () => {
+  console.log('Server is running on http://localhost:8080');
+});
+
+
+
 const Room = class {
-  constructor(id, codenames) {
+  constructor(id, codenames, coloring) {
     this.id = id;
     this.members = [];
+    this.admins = [];
     this.codenames = codenames;
+    this.coloring = coloring;
   }
 
-  add_user(id) {
-    if (this.members.includes(id)) {
-      console.log(`! ROOM[ ${this.id} ] already includes USER[ ${id} ]`);
-    } else {
+  is_member(id) { return this.members.includes(id); }
+
+  add_user(id) { 
+    if (!this.is_member(id)) {
       this.members.push(id);
     }
   }
 
   remove_user(id) {
-    if (!this.members.includes(id)) {
-      console.log(`! ROOM[ ${this.id} ] did not even include USER[ ${id} ]`);
-    } else {
+    if (this.is_member(id)) {
       this.members = this.members.filter((m) => m != id);
+    }
+  }
+
+  is_admin(id) { return this.admins.includes(id); }
+
+  add_admin(id) {
+    if (this.is_member(id) && !this.is_admin(id)) {
+      this.admins.push(id);
+    }
+  }
+
+  remove_admin(id) {
+    if (this.is_admin(id)) {
+      this.admins = this.admins.filter((m) => m != id);
     }
   }
 
 };
 
-const Rooms = class {
+const RoomManager = class {
   constructor() {
     this.rooms = {};
   }
 
-  add_room(room) {
-    console.log(`> create ROOM[ ${room.id} ]`);
-    this.rooms[room.id] = room;
-  }
+  is_room(id) { return this.rooms[id]; }
 
-  user_exists_in_room(user_id, room_id) {
-    return this.room_exists(room_id) && this.rooms[room_id].members.includes(user_id)
-  }
-
-  room_exists(id) {
-    return this.rooms[id]
-  }
+  add_room(room) { this.rooms[room.id] = room; }
 
   remove_room(id) {
     if (id == null) { return }
-    console.log(`> remove ROOM[ ${id} ]`);
     delete this.rooms[id];
   }
 
+
+
+  is_user_in_room(user_id, room_id) { return this.room_exists(room_id) && this.rooms[room_id].is_member(user_id); }
+
   add_user_to_room(user_id, room_id) {
     if (user_id == null || room_id == null) { return }
-    console.log(`> add USER[ ${user_id} ] to ROOM[ ${room_id} ] `);
     this.rooms[room_id].add_user(user_id);
   }
 
   remove_user_from_room(user_id, room_id) {
     if (user_id == null || room_id == null) { return }
-    console.log(`> remove USER[ ${user_id} ] from ROOM[ ${room_id} ] `);
     this.rooms[room_id].remove_user(user_id);
   }
+
+
+
+  is_admin_in_room(user_id, room_id) { return this.is_room(room_id) && this.rooms[room_id].is_admin(user_id); }
+
+  add_admin_to_room(admin_id, user_id, room_id) {
+    if (user_id == null || room_id == null) { return }
+    if (!this.is_admin_in_room(admin_id, room_id) && this.rooms[room_id].admins.length > 0) { return }
+    this.rooms[room_id].add_user(user_id);
+    this.rooms[room_id].add_admin(user_id);
+  }
+
+  remove_admin_from_room(admin_id, user_id, room_id) {
+    if (user_id == null || room_id == null) { return }
+    if (!this.is_admin_in_room(admin_id, room_id)) { return }
+    this.rooms[room_id].remove_admin(user_id);
+
+    console.log(this.rooms[room_id].admins)
+
+    for (const id of Object.entries(this.rooms).filter(([_, room]) => room.admins.length == 0).map(([id, _]) => id)) {
+      console.log(this.rooms[id]);
+      room_manager.add_admin_to_room("mr. bunny", this.rooms[id].members[0], id);
+    }
+  }
+
+
 
   remove_empty_rooms() {
     for (const id of Object.entries(this.rooms).filter(([_, room]) => room.members.length == 0).map(([id, _]) => id)) {
       this.remove_room(id);
     }
   }
-
+  
 }
 
-const rooms = new Rooms();
 
+
+const room_manager = new RoomManager();
 
 io.on('connection', (socket) => {
-  console.log('A user connected.');
 
-  let new_user_id = null;
-  let new_room_id = null;
+  var user_id = socket.id;
+  var room_id = "bunny";
+  var user_name = "incognito";
+
+  console.log(`A user [${format_user()}] connected.`);   
+
+  function format_user() {
+    return user_name+"#"+user_id.slice(0,5);
+  }
+  
+  function leave_room() {
+    room_manager.remove_user_from_room(format_user(), room_id);
+    
+    socket.leave(room_id);
+    io.to(room_id).emit('announcement', {'message': `A user [${format_user()}] left your room [${room_id}]`});
+    io.to(room_id).emit('roomUpdate', room_manager.rooms[room_id]);
+  }
+
+  function join_room() {
+    if (!room_manager.is_room(room_id)) {
+      const data = fs.readFileSync('public/original.txt', 'utf8');
+      const codenames = data.split('\n').sort(() => 0.5 - Math.random()).slice(0, 25);
+      // assassin is coloring[0]
+      // red cards is coloring[1..9]
+      // blue cards is coloring[10..18]
+      // neutral is coloring[19..25]
+      const coloring = [...Array(25).keys()].sort(() => 0.5 - Math.random())
+      room_manager.add_room(new Room(room_id, codenames, coloring));
+      room_manager.add_admin_to_room("mr. bunny", format_user(), room_id);
+
+    } else {
+      room_manager.add_user_to_room(format_user(), room_id);
+    }
+    
+    socket.emit('userUpdate', {"user": format_user(), "room": room_id});
+
+    socket.join(room_id);
+
+    io.to(room_id).emit('announcement', {'message': `A user [${format_user()}] joined your room [${room_id}]`});
+    io.to(room_id).emit('roomUpdate', room_manager.rooms[room_id]);
+  }
+
+  join_room();
 
   socket.on('joinRoom', (dataObject) => {
 
-    if (rooms.user_exists_in_room(dataObject.user_id, dataObject.room_id)) {
-      return
+    leave_room();
+
+    room_id = dataObject.room_id;
+    user_name = dataObject.user_name;
+    join_room();   
+
+    room_manager.remove_empty_rooms();
+
+  });
+
+  socket.on('makeAdmin', (user_to_be_made_admin) => {
+
+    if (format_user() != user_to_be_made_admin && room_manager.is_admin_in_room(user_to_be_made_admin, room_id)){
+      room_manager.remove_admin_from_room(format_user(), user_to_be_made_admin, room_id);
+    } else {
+      room_manager.add_admin_to_room(format_user(), user_to_be_made_admin, room_id);
     }
-
-    prev_user_id = new_user_id;
-    prev_room_id = new_room_id;
-    new_user_id = dataObject.user_id;
-    new_room_id = dataObject.room_id;
-
-    console.log(`--> ${prev_user_id}@${prev_room_id} to ${new_user_id}@${new_room_id}`);
-
-    rooms.remove_user_from_room(prev_user_id, prev_room_id);
-    socket.leave(prev_room_id);
-
-    if (!rooms.room_exists(new_room_id)) {
-      const data = fs.readFileSync('public/original.txt', 'utf8');
-      const randomLines = data.split('\n').sort(() => 0.5 - Math.random())
-      const randomLines25 = randomLines.slice(0, 25);
-      rooms.add_room(new Room(new_room_id, randomLines25));
-    }
-
-    rooms.add_user_to_room(new_user_id, new_room_id);
-    socket.join(new_room_id);
-
-    rooms.remove_empty_rooms();
-
-    io.to(prev_room_id).emit('message', {'user_id': '!', 'message': `ANNOUNCEMENT: A user [${prev_user_id}] left your room [${prev_room_id}]`});
-    io.to(new_room_id).emit('message', {'user_id': '!', 'message': `ANNOUNCEMENT: A user [${new_user_id}] joined your room [${new_room_id}]`});
-
-    socket.emit('roomData', rooms.rooms[new_room_id].codenames);
-
+    
+    io.to(room_id).emit('roomUpdate', room_manager.rooms[room_id]);
   });
 
   socket.on('disconnect', () => {
-    
-    rooms.remove_user_from_room(new_user_id, new_room_id);
-    io.to(new_room_id).emit('message', {'user_id': '!', 'message': `ANNOUNCEMENT: A user [${new_user_id}] left your room [${new_room_id}]`});
+    console.log(`A user [${format_user()}] disconnected.`);   
+    leave_room()
 
-    console.log(`A user [${new_user_id}] disconnected.`);   
+    room_manager.remove_admin_from_room(format_user(), format_user(), room_id);
+    room_manager.remove_empty_rooms();
+
   });
 
 });
 
-server.listen(8080, () => {
-  console.log('Server is running on http://localhost:8080');
-});
+
+
