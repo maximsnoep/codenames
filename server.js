d045d265a975ed676dacd38184f1fb54ee203319
Snoep <ik@maximsnoep.com>
2024-03-18 23:22:32 +0100

    can reveal cards now, including reveal all, or just click a single card, can also reinit games

--- server.js
+++ server.js
@@ -23,36 +23,31 @@ const Room = class {
     this.id = id;
     this.members = [];
     this.admins = [];
-    this.codenames = codenames;
-    this.coloring = coloring;
+    this.codenames = [];
+    this.coloring = [];
+    this.revealed = [];
   }
 
+  // member management
   is_member(id) { return this.members.includes(id); }
+  add_user(id) { !this.is_member(id) && this.members.push(id); }
+  remove_user(id) { this.members = this.members.filter((m) => m != id); }
 
-  add_user(id) { 
-    if (!this.is_member(id)) {
-      this.members.push(id);
-    }
-  }
-
-  remove_user(id) {
-    if (this.is_member(id)) {
-      this.members = this.members.filter((m) => m != id);
-    }
-  }
-
+  // admin management
   is_admin(id) { return this.admins.includes(id); }
+  add_admin(id) { if (this.is_member(id) && !this.is_admin(id)) this.admins.push(id); }
+  remove_admin(id) { this.admins = this.admins.filter((m) => m != id); }
 
-  add_admin(id) {
-    if (this.is_member(id) && !this.is_admin(id)) {
-      this.admins.push(id);
-    }
-  }
-
-  remove_admin(id) {
-    if (this.is_admin(id)) {
-      this.admins = this.admins.filter((m) => m != id);
-    }
+  // game management
+  reveal_card(id) { this.revealed[id] = true; }
+  fill() {
+    this.codenames = fs.readFileSync('public/original.txt', 'utf8').split('\n').sort(() => 0.5 - Math.random()).slice(0, 25);
+    // assassin is coloring[0]
+    // red cards is coloring[1..9]
+    // blue cards is coloring[10..18]
+    // neutral is coloring[19..25]
+    this.coloring = [...Array(25).keys()].sort(() => 0.5 - Math.random());
+    this.revealed = Array(25).fill(false);
   }
 
 };
@@ -62,32 +57,17 @@ const RoomManager = class {
     this.rooms = {};
   }
 
-  is_room(id) { return this.rooms[id]; }
-
-  add_room(room) { this.rooms[room.id] = room; }
-
-  remove_room(id) {
-    if (id == null) { return }
-    delete this.rooms[id];
+  remove_empty_rooms() {
+    for (const id of Object.entries(this.rooms).filter(([_, room]) => room.members.length == 0).map(([id, _]) => id)) {
+      id && delete this.rooms[id];
+    }
   }
 
-
-
+  // user management
   is_user_in_room(user_id, room_id) { return this.room_exists(room_id) && this.rooms[room_id].is_member(user_id); }
 
-  add_user_to_room(user_id, room_id) {
-    if (user_id == null || room_id == null) { return }
-    this.rooms[room_id].add_user(user_id);
-  }
-
-  remove_user_from_room(user_id, room_id) {
-    if (user_id == null || room_id == null) { return }
-    this.rooms[room_id].remove_user(user_id);
-  }
-
-
-
-  is_admin_in_room(user_id, room_id) { return this.is_room(room_id) && this.rooms[room_id].is_admin(user_id); }
+  // admin management
+  is_admin_in_room(user_id, room_id) { return this.rooms[room_id] && this.rooms[room_id].is_admin(user_id); }
 
   add_admin_to_room(admin_id, user_id, room_id) {
     if (user_id == null || room_id == null) { return }
@@ -110,11 +90,10 @@ const RoomManager = class {
   }
 
 
-
-  remove_empty_rooms() {
-    for (const id of Object.entries(this.rooms).filter(([_, room]) => room.members.length == 0).map(([id, _]) => id)) {
-      this.remove_room(id);
-    }
+  // game management
+  reveal_card(room_id, card_id) {
+    if (room_id == null || card_id == null) { return }
+    this.rooms[room_id].reveal_card(card_id);
   }
   
 }
@@ -125,80 +104,73 @@ const room_manager = new RoomManager();
 
 io.on('connection', (socket) => {
 
-  var user_id = socket.id;
-  var room_id = "bunny";
-  var user_name = "incognito";
+  console.log(`A socket [${socket.id}] connected.`);
 
-  console.log(`A user [${format_user()}] connected.`);   
-
-  function format_user() {
-    return user_name+"#"+user_id.slice(0,5);
-  }
+  var user_id;
+  var room_id;
   
   function leave_room() {
-    room_manager.remove_user_from_room(format_user(), room_id);
-    
+    console.log(`<${user_id}> left <${room_id}>`);  
+    if (user_id && room_id) room_manager.rooms[room_id].remove_user(user_id);
+    if (user_id && room_id) room_manager.remove_admin_from_room(user_id, user_id, room_id);
+    room_manager.remove_empty_rooms();
+
     socket.leave(room_id);
-    io.to(room_id).emit('announcement', {'message': `A user [${format_user()}] left your room [${room_id}]`});
+    io.to(room_id).emit('announcement', {'message': `A user [${user_id}] left your room [${room_id}]`});
     io.to(room_id).emit('roomUpdate', room_manager.rooms[room_id]);
   }
 
   function join_room() {
-    if (!room_manager.is_room(room_id)) {
-      const data = fs.readFileSync('public/original.txt', 'utf8');
-      const codenames = data.split('\n').sort(() => 0.5 - Math.random()).slice(0, 25);
-      // assassin is coloring[0]
-      // red cards is coloring[1..9]
-      // blue cards is coloring[10..18]
-      // neutral is coloring[19..25]
-      const coloring = [...Array(25).keys()].sort(() => 0.5 - Math.random())
-      room_manager.add_room(new Room(room_id, codenames, coloring));
-      room_manager.add_admin_to_room("mr. bunny", format_user(), room_id);
-
+    if (!room_manager.rooms[room_id]) {
+      console.log(`<${user_id}> created <${room_id}>`);
+      room_manager.rooms[room_id] = new Room(room_id)
+      room_manager.rooms[room_id].fill();
+      room_manager.add_admin_to_room("mr. bunny", user_id, room_id);
     } else {
-      room_manager.add_user_to_room(format_user(), room_id);
+      console.log(`<${user_id}> joined <${room_id}>`);
+      if (user_id && room_id) room_manager.rooms[room_id].add_user(user_id);
     }
     
-    socket.emit('userUpdate', {"user": format_user(), "room": room_id});
+    socket.emit('userUpdate', {"user": user_id, "room": room_id});
 
     socket.join(room_id);
-
-    io.to(room_id).emit('announcement', {'message': `A user [${format_user()}] joined your room [${room_id}]`});
+    io.to(room_id).emit('announcement', {'message': `A user [${user_id}] joined your room [${room_id}]`});
     io.to(room_id).emit('roomUpdate', room_manager.rooms[room_id]);
   }
 
-  join_room();
+  
 
   socket.on('joinRoom', (dataObject) => {
-
     leave_room();
-
     room_id = dataObject.room_id;
-    user_name = dataObject.user_name;
-    join_room();   
-
-    room_manager.remove_empty_rooms();
-
+    user_id = dataObject.user_name + "#" + Math.floor(Math.random() * 10000).toString().padStart(4, '0');
+    join_room();       
   });
 
   socket.on('makeAdmin', (user_to_be_made_admin) => {
-
-    if (format_user() != user_to_be_made_admin && room_manager.is_admin_in_room(user_to_be_made_admin, room_id)){
-      room_manager.remove_admin_from_room(format_user(), user_to_be_made_admin, room_id);
+    if (user_id != user_to_be_made_admin && room_manager.is_admin_in_room(user_to_be_made_admin, room_id)){
+      room_manager.remove_admin_from_room(user_id, user_to_be_made_admin, room_id);
     } else {
-      room_manager.add_admin_to_room(format_user(), user_to_be_made_admin, room_id);
+      room_manager.add_admin_to_room(user_id, user_to_be_made_admin, room_id);
     }
     
     io.to(room_id).emit('roomUpdate', room_manager.rooms[room_id]);
   });
 
   socket.on('disconnect', () => {
-    console.log(`A user [${format_user()}] disconnected.`);   
     leave_room()
+  });
 
-    room_manager.remove_admin_from_room(format_user(), format_user(), room_id);
-    room_manager.remove_empty_rooms();
+  socket.on('revealCards', (card_ids) => {
+    for (const card_id of card_ids) {
+      room_manager.reveal_card(room_id, card_id);
+    }
+    io.to(room_id).emit('roomUpdate', room_manager.rooms[room_id]);
+  });
 
+  socket.on('reinitGame', () => {
+    room_manager.rooms[room_id].fill();
+    io.to(room_id).emit('roomUpdate', room_manager.rooms[room_id]);
   });
 
 });
